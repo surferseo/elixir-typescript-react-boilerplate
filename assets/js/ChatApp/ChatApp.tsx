b7cb0980424ec7997b6ea2099c593ad6ac3886b0
Lucjan Suski <lucjansuski@gmail.com>
2020-10-16 17:47:09 +0200

    initial commit

---
+++ assets/js/ChatApp/ChatApp.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const ChatApp = () => {
+  return <div>Build chat here</div>
+}
---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :chat,
+  ecto_repos: [Chat.Repo]
+
+# Configures the endpoint
+config :chat, ChatWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "aAkfPpVNiAV7SMs8i2Kq/MYpzh//0KIUp46TyuWH3KyD2HH5KY3ucNRrlJ0FVzwb",
+  render_errors: [view: ChatWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Chat.PubSub,
+  live_view: [signing_salt: "uSqPG682"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
